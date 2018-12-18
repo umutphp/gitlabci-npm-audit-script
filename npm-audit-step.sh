8c5a140d517696aec5becbcb86339b0eccc2c6ee
Umut Işık <umutphp@gmail.com>
2018-12-19 00:23:41 +0300

    Initial commit

---
+++ npm-audit-step.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+print_vulnerabilities () {
+    ./jq-linux64 .metadata.vulnerabilities < ./audit_result.json
+    cat ./ui/audit_result.json
+}
+
+wget https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
+chmod +x jq-linux64
+npm i --package-lock-only
+npm audit --json > audit_result.json
+
+INFO_VUL="$(./jq-linux64 .metadata.vulnerabilities.info < ./audit_result.json)"
+LOW_VUL="$(./jq-linux64 .metadata.vulnerabilities.low < ./audit_result.json)"
+MODERATE_VUL="$(./jq-linux64 .metadata.vulnerabilities.moderate < ./audit_result.json)"
+HIGH_VUL="$(./jq-linux64 .metadata.vulnerabilities.high < ./audit_result.json)"
+CRITICAL_VUL="$(./jq-linux64 .metadata.vulnerabilities.critical < ./audit_result.json)"
+
+if [ "$INFO_VUL" -ne "0" ]
+then
+    print_vulnerabilities
+    exit 1
+fi
+
+if [ "$LOW_VUL" -ne "0" ]
+then
+    print_vulnerabilities
+    exit 1
+fi
+
+if [ "$MODERATE_VUL" -ne "0" ]
+then
+    print_vulnerabilities
+    exit 1
+fi
+
+if [ "$HIGH_VUL" -ne "0" ]
+then
+    print_vulnerabilities
+    exit 1
+fi
+
+if [ "$CRITICAL_VUL" -ne "0" ]
+then
+    print_vulnerabilities
+    exit 1
+fi
